8ebd2deadc3a978e45dd088390152fbd20350122
Tyuiu.PanasenkoRA@outlook.com <panasenkoroma@mail.ru>
2023-10-12 15:25:56 +0500

    Task4

---
+++ Tyuiu.PanasenkoRA.Sprint1.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.PanasenkoRA.Sprint1.Task4.V0.Lib
+{
+    public class DataService : ISprint1Task4V9
+    {
+        public double Calculate(double x, double y)
+        {
+            var res = Math.Round(Math.Log(x * y, Math.E) / (x - Math.Sqrt(1 + Math.Pow(y, 2))), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.PanasenkoRA.Sprint1.Task4.V0/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PanasenkoRA.Sprint1.Task4.V0.Lib;
+
+namespace Tyuiu.PanasenkoRA.Sprint1.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Панасенко Р. А. | ПКТб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы с C#                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Панаcенко Роман Анатольевич | ПКТб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y;
+
+            Console.WriteLine("Введите значени Х:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значени Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Ln(x*y) / (x - sqrt(1 + y^2) = " + ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
