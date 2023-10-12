e5f7baad031ce42bfc62e65b69afc40403976ba3
Tyuiu.PanasenkoRA@outlook.com <panasenkoroma@mail.ru>
2023-10-12 16:17:59 +0500

    Task5

---
+++ Tyuiu.PanasenkoRA.Sprint1.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PanasenkoRA.Sprint1.Task5.V0.Lib;
+
+namespace Tyuiu.PanasenkoRA.Sprint1.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            int k = 1234;
+            DataService ds = new DataService();
+            int res = ds.Calculate(k);
+            
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.PanasenkoRA.Sprint1.Task5.V0/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PanasenkoRA.Sprint1.Task5.V0.Lib;
+
+namespace Tyuiu.PanasenkoRA.Sprint1.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Панасенко Р. А. | ПКТб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы с C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Панаcенко Роман Анатольевич | ПКТб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Присвоить целой переменной h третью от конца цифру в записи             *");
+            Console.WriteLine("* положительного целого числа k                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите число К:");
+            int k;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = Convert.ToInt32(ds.Calculate(k));
+        }
+    }
+}
